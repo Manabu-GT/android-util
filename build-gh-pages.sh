fdffc68ceb1f05a2ff5940b94d50c04e2623e533
Manabu-GT <manabu1984+github@gmail.com>
2014-04-11 15:34:11 +0900

    mvn repo build settings

---
+++ build-gh-pages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+HOME_DIR=$PWD
+DIR=$(basename $PWD)-pages
+URL=$(git config --get remote.origin.url)
+
+# Delete local branch pages's directory
+cd ..
+rm -rf $DIR
+
+# Check out pages branch
+git clone $URL $DIR
+
+# Refresh pages branch
+cd $DIR
+PAGES_DIR=$PWD
+
+git checkout --orphan gh-pages
+rm -rf .gitignore
+rm -rf .gitmodules
+rm -rf *
+
+# Create an aar and place it in the pages branch
+cd $HOME_DIR
+./gradlew -Dorg.gradle.project.repoDir="$PAGES_DIR" uploadArchives
+#./gradlew uploadArchives
+
+# Commit pages branch
+git add --all .
+git commit -m "Updated at $(date)"
+
+# Push to gh-pages
+git push origin gh-pages
+
+# Delete pages directory we used
+cd ..
+rm -rf $DIR
