3fe1d3171027e7adaa57a04f39b4bf030b7be350
Manabu-GT <manabu1984+github@gmail.com>
2014-04-11 15:49:37 +0900

    updated build file's comment

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -4,11 +4,11 @@ HOME_DIR=$PWD
 DIR=$(basename $PWD)-pages
 URL=$(git config --get remote.origin.url)
 
-# Delete local branch pages's directory
+# Delete local branch gh-pages's directory
 cd ..
 rm -rf $DIR
 
-# Check out pages branch
+# Check out gh-pages branch
 git clone $URL $DIR
 
 # Clear pages branch history
@@ -20,18 +20,18 @@ rm -rf .gitignore
 rm -rf .gitmodules
 rm -rf *
 
-# Create an aar and place it in the pages branch
+# Create an aar and place it in the gh-pages branch
 cd $HOME_DIR
 ./gradlew -Dorg.gradle.project.repoDir="$PAGES_DIR" uploadArchives
 
-# Commit pages branch
+# Commit to the gh-pages branch
 cd $PAGES_DIR
 git add --all .
 git commit -m "Updated at $(date)"
 
-# Push to gh-pages
+# Push to the gh-pages branch
 git push origin gh-pages
 
-# Delete pages directory we used
+# Delete the gh-pages directory we used
 cd ..
 rm -rf $DIR
