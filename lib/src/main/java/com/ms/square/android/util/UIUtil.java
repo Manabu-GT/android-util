dd60742d8dffe24fe45dbb6e3371c09d1b0e1674
Manabu-GT <manabu1984+github@gmail.com>
2014-06-13 06:04:09 +0900

    updated UIUtil class

--- lib/src/main/java/com/ms/square/android/util/UIUtil.java
+++ lib/src/main/java/com/ms/square/android/util/UIUtil.java
@@ -2,11 +2,18 @@ package com.ms.square.android.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Point;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class UIUtil {
+
+    private static final String TAG = UIUtil.class.getSimpleName();
 
     public static float convertToPixelFromDip(Context ctx, float dip) {
         float pixel = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip,
@@ -25,5 +32,36 @@ public class UIUtil {
         Point size = new Point();
         display.getSize(size);
         return size;
+    }
+
+    public static Bitmap drawViewToBitmap(View view, int downSampling) {
+        return drawViewToBitmap(view, 0f, 0f, downSampling);
+    }
+
+    public static Bitmap drawViewToBitmap(View view, float translateX, float translateY, int downSampling) {
+        float scale = 1f / downSampling;
+        int width = view.getWidth();
+        int height = view.getHeight();
+        int bmpWidth = (int) (width * scale - translateX/downSampling);
+        int bmpHeight = (int) (height * scale - translateY/downSampling);
+        Bitmap dest = Bitmap.createBitmap(bmpWidth, bmpHeight, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(dest);
+        c.translate(-translateX/downSampling, -translateY/downSampling);
+        if (downSampling > 1) {
+            c.scale(scale, scale);
+        }
+        view.draw(c);
+        return dest;
+    }
+
+    // for debugging purpose
+    public static void dumpViewTree(View v, String padding){
+        Log.d(TAG, padding + v.getClass().getName());
+        if(v instanceof ViewGroup){
+            ViewGroup g = (ViewGroup) v;
+            for(int i = 0; i < g.getChildCount(); i++){
+                dumpViewTree(g.getChildAt(i), padding + " ");
+            }
+        }
     }
 }
